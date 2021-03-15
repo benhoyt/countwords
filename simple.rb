99933fad4916740799ca86a96186d833472b56af
Bill Mill <bill@billmill.org>
2021-03-15 12:09:18 -0400

    consistent formatting

--- simple.rb
+++ simple.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 counts = Hash.new(0)
-STDIN.readlines.map { |line| line.downcase.split.each{ |w| counts[w] += 1 }}
-counts.sort_by{|k,v| -v}.each{|k,v| puts "#{k} #{v}"}
+STDIN.readlines.map { |line| line.downcase.split.each { |w| counts[w] += 1 }}
+counts.sort_by { |k,v| -v }.each { |k,v| puts "#{k} #{v}" }
