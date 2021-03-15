4f38e6d532af4b555c6eb296382cd9f729d8dcdb
Roman Garanin <me@romangaranin.dev>
2021-03-15 22:51:48 +0300

    feat: add simple kotlin version, with gradle and uber jar
    it can be solved easier, with just one file with main method only, but we need kotlin stdlib compiled/added to resulting jar, otherwise w/o classed in classpath at runtime it will fail.
    so it is 'easier' to make simple gradle-based project, add uber jar plugin, which will add all dependencies in result jar

---
+++ kotlin/simple/src/main/kotlin/benhoyt/countwords/Simple.kt
@@ -0,0 +1,27 @@
+package benhoyt.countwords
+
+import java.util.*
+import kotlin.collections.HashMap
+
+class Simple {
+    fun solve() {
+        val counts = HashMap<String, Int>()
+
+        val input = Scanner(System.`in`)
+        while (input.hasNextLine()) {
+            val words = input.nextLine().toLowerCase().split(" ")
+            for (word in words) {
+                counts[word] = (counts[word] ?: 0) + 1
+            }
+        }
+
+        val pairs = counts.toList().sortedBy { pair -> pair.second }.reversed()
+        for ((word, count) in pairs) {
+            print("$word $count\n")
+        }
+    }
+}
+
+fun main() {
+    Simple().solve()
+}
