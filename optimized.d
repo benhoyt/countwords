485dc9054314a7499c6b8383a4ccfa2d1e8155bd
Ross Lonstein <ross@develop.lonsteins.com>
2021-03-16 16:12:25 -0400

    squeeze a few more seconds off

--- optimized.d
+++ optimized.d
@@ -1,19 +1,32 @@
 import std.stdio;
 import std.string;
-import ctype = core.stdc.ctype;
-import std.algorithm.iteration : each, splitter;
+import std.algorithm.iteration : splitter;
 import std.algorithm.sorting : schwartzSort;
+
+char mytolower(char c) {
+  mixin("char lc = 'a' - 'A';");
+  if (c < 'A' || c > 'Z') {
+    return c;
+  }
+  return cast(char)(c + lc);
+}
 
 void main() {
   int[string] freq;
   char[] buf;
+
   while (stdin.readln(buf) && !stdin.eof()) {
     foreach (word; splitter(buf)) {
       for (ptrdiff_t idx = 0; idx < word.length; idx++) {
-        //word[idx] = cast(char)word[idx].toLower();
-        word[idx] = cast(char)ctype.tolower(word[idx]);
+        word[idx] = mytolower(word[idx]);
       }
-      freq[ cast(immutable char[])word.dup ]++;
+      auto key = cast(immutable char[])word.dup;
+      if (auto ptr = (key in freq)) {
+        ++*ptr;
+      }
+      else {
+        freq[key] = 1;
+      }
     }
   }
 
