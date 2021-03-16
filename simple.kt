5fb2322f590c03324d35454f75412d35f5083121
Roman Garanin <me@romangaranin.dev>
2021-03-16 10:26:03 +0300

    feat: kotlin remove main class

--- simple.kt
+++ simple.kt
@@ -1,27 +1,21 @@
 import java.util.*
 import kotlin.collections.HashMap
 
-class Simple {
-    fun solve() {
-        val counts = HashMap<String, Int>()
-
-        val input = Scanner(System.`in`)
-        while (input.hasNextLine()) {
-            val tokenizer = StringTokenizer(input.nextLine())
-            while (tokenizer.hasMoreTokens()) {
-                val word = tokenizer.nextToken()
-                counts[word] = (counts[word] ?: 0) + 1
-            }
-        }
-
-        val pairs = counts.toList().sortedByDescending { pair -> pair.second }
-        for ((word, count) in pairs) {
-            print("$word $count\n")
-        }
-    }
-}
-
 
 fun main() {
-    Simple().solve()
+    val counts = HashMap<String, Int>()
+
+    val input = Scanner(System.`in`)
+    while (input.hasNextLine()) {
+        val tokenizer = StringTokenizer(input.nextLine())
+        while (tokenizer.hasMoreTokens()) {
+            val word = tokenizer.nextToken()
+            counts[word] = (counts[word] ?: 0) + 1
+        }
+    }
+
+    val pairs = counts.toList().sortedByDescending { pair -> pair.second }
+    for ((word, count) in pairs) {
+        print("$word $count\n")
+    }
 }
