2261d5d2d93fe70286eeafe8d490fc50750940cb
Ross Lonstein <ross@develop.lonsteins.com>
2021-03-16 13:43:11 -0400

    go fast, take chances...

--- optimized.d
+++ optimized.d
@@ -1,5 +1,6 @@
 import std.stdio;
 import std.string;
+import ctype = core.stdc.ctype;
 import std.algorithm.iteration : each, splitter;
 import std.algorithm.sorting : schwartzSort;
 
@@ -9,9 +10,10 @@ void main() {
   while (stdin.readln(buf) && !stdin.eof()) {
     foreach (word; splitter(buf)) {
       for (ptrdiff_t idx = 0; idx < word.length; idx++) {
-        word[idx] = cast(char)word[idx].toLower();
+        //word[idx] = cast(char)word[idx].toLower();
+        word[idx] = cast(char)ctype.tolower(word[idx]);
       }
-      freq[word.idup]++;
+      freq[ cast(immutable char[])word.dup ]++;
     }
   }
 
