eddf251f5e565e9d7c9fd7db540c29f939526c16
Ben Hoyt <benhoyt@gmail.com>
2021-03-17 21:44:16 +1300

    Shorten credits in table so it fits on one line

--- benchmark.py
+++ benchmark.py
@@ -29,7 +29,7 @@ programs = [
     ('C++', './simple-cpp', './optimized-cpp', 'optimized by Jussi Pakkanen'),
     ('Python', 'python3 simple.py', 'python3 optimized.py', ''),
     ('Ruby', 'ruby simple.rb', 'ruby optimized.rb', 'by Bill Mill'),
-    ('C#', './csharp/simple/bin/Release/net5.0/simple', './csharp/optimized/bin/Release/net5.0/optimized', 'by John Taylor, Yuriy Ostapenko, Osman Turan'),
+    ('C#', './csharp/simple/bin/Release/net5.0/simple', './csharp/optimized/bin/Release/net5.0/optimized', 'by J Taylor, Y Ostapenko, O Turan'),
     ('AWK', 'gawk -f simple.awk', 'mawk -f optimized.awk', 'optimized uses `mawk`'),
     ('Forth', '../gforth/gforth-fast simple.fs', '../gforth/gforth-fast optimized.fs', ''),
     ('Shell', 'bash simple.sh', 'bash optimized.sh', 'optimized does `LC_ALL=C sort -S 2G`'),
