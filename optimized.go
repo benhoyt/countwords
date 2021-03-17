3cd9af31f4ec66e1a352d7ce1dd3934b3c3f4497
Miguel Angel <ntrrgx@gmail.com>
2021-03-17 18:10:46 -0400

    Fix optimized.go inconsistency
    Hi! I don't know if its just me, but I am actually getting inconsistent outputs every time I run it.
    
    ```
    jiphthahel 9
    jip 1
    h 1
    sait 1
    t 1
    hthahel 1
    ```
    
    This version is a little bit slower, but works. And it is pretty easy to understand and maintain if I may say (more than the simple version :sweat_smile:).

--- optimized.go
+++ optimized.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -9,91 +8,77 @@ import (
 )
 
 func main() {
-	offset := 0
-	buf := make([]byte, 64*1024)
-	counts := make(map[string]*int)
-	for {
-		// Read input in 64KB blocks till EOF.
-		n, err := os.Stdin.Read(buf[offset:])
-		if err != nil && err != io.EOF {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		if n == 0 {
-			break
-		}
-		// Offset remaining from last time plus number of bytes read.
-		chunk := buf[:offset+n]
+	wc := NewWordCounter()
+	io.Copy(wc, os.Stdin)
 
-		// Find last end-of-line character in block read.
-		lastLF := bytes.LastIndexByte(chunk, '\n')
-		toProcess := chunk
-		if lastLF != -1 {
-			toProcess = chunk[:lastLF]
-		}
-
-		// Loop through toProcess slice and count words.
-		start := -1 // start -1 means in whitespace run
-		for i, c := range toProcess {
-			// Convert to ASCII lowercase in place as we go.
-			if c >= 'A' && c <= 'Z' {
-				c = c + ('a' - 'A')
-				toProcess[i] = c
-			}
-			if start >= 0 {
-				// In a word, look for end of word (whitespace).
-				if c <= ' ' {
-					// Count this word!
-					increment(counts, toProcess[start:i])
-					start = -1
-				}
-			} else {
-				// In whitespace, look for start of word (non-space).
-				if c > ' ' {
-					start = i
-				}
-			}
-		}
-		// Count last word, if any.
-		if start >= 0 && start < len(toProcess) {
-			increment(counts, toProcess[start:])
-		}
-
-		// Copy remaining bytes (incomplete line) to start of buffer.
-		if lastLF != -1 {
-			remaining := chunk[lastLF+1:]
-			copy(buf, remaining)
-			offset = len(remaining)
-		} else {
-			offset = 0
-		}
-	}
-
-	var ordered []Count
-	for word, count := range counts {
-		ordered = append(ordered, Count{word, *count})
-	}
-	sort.Slice(ordered, func(i, j int) bool {
-		return ordered[i].Count > ordered[j].Count
-	})
-
-	for _, count := range ordered {
-		fmt.Println(string(count.Word), count.Count)
+	for _, word := range wc.MostCommon() {
+		fmt.Println(word.Word, word.Count)
 	}
 }
 
-func increment(counts map[string]*int, word []byte) {
-	if p, ok := counts[string(word)]; ok {
-		// Word already in map, increment existing int via pointer.
-		*p++
-		return
-	}
-	// Word not in map, insert new int.
-	n := 1
-	counts[string(word)] = &n
-}
-
-type Count struct {
+type WordCount struct {
 	Word  string
 	Count int
+}
+
+type WordCounter struct {
+	buf   []byte
+	words map[string]*int
+}
+
+func NewWordCounter() *WordCounter {
+	return &WordCounter{
+		words: make(map[string]*int),
+	}
+}
+
+func (wc *WordCounter) addWord() {
+	if len(wc.buf) == 0 {
+		return
+	}
+
+	// Clean buffer after counting a word
+	defer func() { wc.buf = wc.buf[:0] }()
+
+	if n, ok := wc.words[string(wc.buf)]; ok {
+		*n++
+		return
+	}
+
+	n := 1
+	wc.words[string(wc.buf)] = &n
+}
+
+func (wc *WordCounter) MostCommon() []WordCount {
+	// Flush any data in the buffer
+	wc.addWord()
+
+	counts := make([]WordCount, 0, len(wc.words))
+	for word, count := range wc.words {
+		counts = append(counts, WordCount{word, *count})
+	}
+
+	sort.Slice(counts, func(i, j int) bool {
+		return counts[i].Count > counts[j].Count
+	})
+
+	return counts
+}
+
+func (wc *WordCounter) Write(p []byte) (int, error) {
+	for _, b := range p {
+		if b == ' ' || b == '\n' {
+			wc.addWord()
+			continue
+		}
+
+		// To lower case
+		if b >= 'A' && b <= 'Z' {
+			b += 'a' - 'A'
+		}
+
+		wc.buf = append(wc.buf, b)
+	}
+
+	return len(p), nil
 }
