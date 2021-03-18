7a551e05247154e0afd5bcdf21fb182a9ec2886f
RUSshy <ismaalove@gmail.com>
2021-03-18 02:33:48 +0100

    Use LDC compiler for D benchmark (#84)
    Hello
    
    LDC is the recommended compiler for release mode / benchmarks
    
    DMD is recommended for fast compile time for development purposes, not suited for benchmarks
    
    
    DMD
    ```
    Exit code      : 0
    Elapsed time   : 0.22
    Kernel time    : 0.02 (7.1%)
    User time      : 0.20 (92.3%)
    page fault #   : 2984
    Working set    : 11344 KB
    Paged pool     : 123 KB
    Non-paged pool : 9 KB
    Page file size : 14992 KB
    ```
    
    LDC
    ```
    Exit code      : 0
    Elapsed time   : 0.15
    Kernel time    : 0.03 (20.2%)
    User time      : 0.12 (80.8%)
    page fault #   : 3644
    Working set    : 14164 KB
    Paged pool     : 51 KB
    Non-paged pool : 6 KB
    Page file size : 14504 KB
    ```

--- test.sh
+++ test.sh
@@ -165,12 +165,12 @@ node ./optimized <kjvbible_x10.txt | python3 normalize.py >output.txt
 git diff --exit-code output.txt
 
 echo DLang simple
-dmd -release -O -inline -of=simple-d simple.d
+ldc2 -release -O3 -of=simple-d simple.d
 ./simple-d <kjvbible_x10.txt | python3 normalize.py >output.txt
 git diff --exit-code output.txt
 
 echo DLang optimized
-dmd -release -O -inline -of=optimized-d optimized.d
+ldc2 -release -O3 -of=optimized-d optimized.d
 ./optimized-d <kjvbible_x10.txt | python3 normalize.py >output.txt
 git diff --exit-code output.txt
 
