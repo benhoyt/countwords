7e03553b6fb29462bfb1d8c24735b963d74fbf83
Jesse Hathaway <jesse@mbuki-mvuki.org>
2021-03-19 14:31:41 -0500

    three optimizations
    > * «while LANG=C IFS= read ...; do ...» is slow because Bash
    > reinitializes the locales of the C library every time any shell
    > variables LANG or LC_* is changed. One can instead set LANG outside
    > the while statement and restore it after the while statement.
    
    saves ~7s
    
    don't quote associative array subscript
    
    saves ~2s
    
    use a sparse array and then compact for sorting
    
    saves ~1s
    
    mailing list discussion:
    
    https://lists.gnu.org/archive/html/help-bash/2021-03/threads.html#00198
    
    Co-authored-by: Koichi Murase <myoga.murase@gmail.com>

--- optimized.bash
+++ optimized.bash
@@ -5,58 +5,37 @@ set -o pipefail
 shopt -s lastpipe
 
 # Count frequencies
+OLD_LANG=$LANG
+LANG='C'
 declare -iA words_to_freq
 eof='false'
 set -o noglob
 while [[ "${eof}" == 'false' ]]; do
-	if ! LANG='C' IFS='' read -N 65536 -r block; then
+	if ! IFS='' read -N 65536 -r b; then
 		eof='true'
 	fi
-	if ! IFS='' read -r line; then
+	if ! IFS='' read -r l; then
 		eof='true'
 	fi
-	for word in ${block@L}${line@L}; do
-		words_to_freq["${word}"]+=1
+	for word in ${b,,}${l,,}; do
+		words_to_freq[$word]+=1
 	done
 done
 set +o noglob
+LANG=$OLD_LANG
 
-# Create a map of unique frequences to tab deliminted words
-declare -A freq_to_words
+# Construct a sparse array with outputs for each freq
+declare -a output
 for word in "${!words_to_freq[@]}"; do
-	freq="${words_to_freq["${word}"]}"
-	if [[ -v "freq_to_words[${freq}]" ]]; then
-		freq_to_words["${freq}"]+=$'\t'"${word}"
-	else
-		freq_to_words["${freq}"]="${word}"
-	fi
+	freq=${words_to_freq[$word]}
+	output[freq]+=$word' '$freq$'\n'
 done
 
-# Create array to sort
-declare -ia freq_sorted
-declare -i i=0
-for freq in "${!freq_to_words[@]}"; do
-	freq_sorted[$i]=$freq
-	i+=1
+# Compact array, then reverse
+output=("${output[@]}")
+((i = 0, j = ${#output[@]} - 1))
+while ((j >= 0)); do
+	r[i++]=${output[j--]}
 done
 
-# Sort frequences
-# https://en.wikipedia.org/wiki/Insertion_sort
-declare -i j
-for ((i = 1; i < ${#freq_sorted[@]}; i++)); do
-	x=${freq_sorted[$i]}
-	j=$((i - 1))
-	while ((j >= 0)) && ((freq_sorted[j] < x)); do
-		freq_sorted[$j + 1]=${freq_sorted[$j]}
-		j=$((j - 1))
-	done
-	freq_sorted[$j + 1]=$x
-done
-
-# Print
-for freq in "${freq_sorted[@]}"; do
-	IFS=$'\t' read -ra words <<<"${freq_to_words[$freq]}"
-	for word in "${words[@]}"; do
-		printf "%s %d\n" "${word}" "${freq}"
-	done
-done
+printf '%s' "${r[@]}"
