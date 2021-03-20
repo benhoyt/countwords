0af48b1425c995ddee479a0af83639b8bbc9a1a0
Anurag Soni <anurag@sonianurag.com>
2021-03-20 00:31:56 -0400

    stick to lists for simple example

--- simple.ml
+++ simple.ml
@@ -19,13 +19,8 @@ let () =
                try incr (StringHashtbl.find countwords word)
                with Not_found -> StringHashtbl.add countwords word (ref 1)))
     done
-  with End_of_file ->
-    let arr = Array.make (StringHashtbl.length countwords) ("", 0) in
-    let idx = ref 0 in
-    StringHashtbl.iter
-      (fun word count ->
-        arr.(!idx) <- (word, !count);
-        incr idx)
-      countwords;
-    Array.sort (fun (_, x) (_, y) -> Int.compare y x) arr;
-    Array.iter (fun (w, c) -> Printf.printf "%s %d\n" w c) arr
+  with
+  | End_of_file ->
+    StringHashtbl.fold (fun k v acc -> (k, v) :: acc) countwords []
+    |> List.sort (fun (_, x) (_, y) -> Int.compare !y !x)
+    |> List.iter (fun (w, c) -> Printf.printf "%s %d\n" w !c)
