d4e99e8a6953b756794a658e0325002e086e6251
arideden <will@arideden.com>
2021-03-23 20:38:29 +0000

    Update simple.tcl
    Fix for input with consecutive whitespace

--- simple.tcl
+++ simple.tcl
@@ -1,10 +1,13 @@
 #!/usr/bin/env tclsh
 fconfigure stdin -buffering line
 while {[gets stdin data] >= 0} {
-  foreach word [string tolower [split $data]] {
+  foreach word [split [string tolower $data]] {
     incr table($word)
   }
 }
+# Remove the count of null elements resulting from the split function
+# being applied to consecutive whitespace in the input
+unset table()
 foreach {word count} [lsort -decreasing -integer -index 1 -stride 2 [array get table]] {
   puts "$word $count"
 }
