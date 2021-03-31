5318b1acdd5bd313039d480af535cf79565c2e62
Ben Hoyt <benhoyt@gmail.com>
2021-04-01 08:46:53 +1300

    Simplify optimized.py (thanks Daniel Fox)

--- optimized.py
+++ optimized.py
@@ -4,10 +4,9 @@ import sys
 counts = collections.Counter()
 remaining = ''
 while True:
-    chunk = sys.stdin.read(64*1024)
+    chunk = remaining + sys.stdin.read(64*1024)
     if not chunk:
         break
-    chunk = remaining + chunk
     last_lf = chunk.rfind('\n')  # process to last LF character
     if last_lf == -1:
         remaining = ''
@@ -15,8 +14,6 @@ while True:
         remaining = chunk[last_lf+1:]
         chunk = chunk[:last_lf]
     counts.update(chunk.lower().split())
-
-counts.update(remaining.lower().split())
 
 for word, count in counts.most_common():
     print(word, count)
