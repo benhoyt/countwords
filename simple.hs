7265a2a58264024e45aa3342ef81e5a547487733
Nycticorax <mrnycticorax@gmail.com>
2021-04-20 21:04:04 +0200

    Simplified.
    Signed-off-by: Nycticorax <mrnycticorax@gmail.com>

--- simple.hs
+++ simple.hs
@@ -1,18 +1,12 @@
-import Data.Char
-import Data.List
+import Data.Char (toLower)
+import Data.List (sortOn)
 import Data.Ord(Down(..))
 import qualified Data.Map as M
 
-countwords :: [String] -> [(String, Int)] 
-countwords [] = []
-countwords ws =
-    let seed = M.empty
-        go mmap [] = sortOn (Down . snd) . M.toList $ mmap
-        go mmap (x:xs) = 
-            let w = map toLower x
-            in  case M.lookup w mmap of
-                Nothing -> go (M.insert w 1 mmap) xs
-                Just v -> go (M.insertWith (\_ _ -> v + 1) w v mmap) xs
-    in  go seed ws
+countwords = go M.empty where
+    go mmap [] = sortOn (Down . snd) . M.toList $ mmap
+    go mmap (x:xs) = let w = map toLower x in case M.lookup w mmap of
+        Nothing -> go (M.insert w 1 mmap) xs
+        Just v -> go (M.insertWith (\_ _ -> v + 1) w v mmap) xs
 
-main = getLine >>= print . show . countwords . words
+main = getLine >>= print . countwords . words
