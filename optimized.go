7731a4ac25bcf422928e7c91972a0d6b3a8c01e9
KevBurnsJr <kevburnsjr@gmail.com>
2021-06-23 22:34:06 -0700

    Further optimize Go implementation (~ 10% improvement)
    - Buffer output
    - Replace int pointer with struct pointer, reducing allocations
    - Remove temporary variables, altering buffer directly

--- optimized.go
+++ optimized.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
@@ -8,12 +9,15 @@ import (
 )
 
 func main() {
-	var word []byte
-	buf := make([]byte, 64*1024)
-	counts := make(map[string]*int)
+	var n int
+	var offset int
+	var remainder int
+	var err error
+	var buf = make([]byte, 64*1024)
+	var counts = map[string]*Count{}
 	for {
 		// Read input in 64KB blocks till EOF.
-		n, err := os.Stdin.Read(buf)
+		n, err = os.Stdin.Read(buf[remainder:])
 		if err != nil && err != io.EOF {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -21,57 +25,54 @@ func main() {
 		if n == 0 {
 			break
 		}
+		n += remainder
+		offset = 0
 
 		// Count words in the buffer.
 		for i := 0; i < n; i++ {
-			c := buf[i]
-
-			// Found a whitespace char, count last word.
-			if c <= ' ' {
-				if len(word) > 0 {
-					increment(counts, word)
-					word = word[:0] // reset word buffer
+			if buf[i] <= ' ' {
+				// Found a whitespace char, count last word.
+				if i > offset {
+					increment(counts, buf[offset:i])
 				}
-				continue
+				offset = i+1
+			} else if buf[i] >= 'A' && buf[i] <= 'Z' {
+				// Convert to ASCII lowercase as we go.
+				buf[i] += 'a' - 'A'
 			}
-
-			// Convert to ASCII lowercase as we go.
-			if c >= 'A' && c <= 'Z' {
-				c = c + ('a' - 'A')
-			}
-
-			// Add non-space char to word buffer.
-			word = append(word, c)
 		}
+		remainder = n-offset
+		copy(buf[:remainder], buf[offset:n])
 	}
 
 	// Count last word, if any.
-	if len(word) > 0 {
-		increment(counts, word)
+	if offset < n {
+		increment(counts, buf[offset:n])
 	}
 
 	// Convert to slice of Count, sort by count descending, print.
-	ordered := make([]Count, 0, len(counts))
-	for word, count := range counts {
-		ordered = append(ordered, Count{word, *count})
+	ordered := make([]*Count, 0, len(counts))
+	for _, count := range counts {
+		ordered = append(ordered, count)
 	}
 	sort.Slice(ordered, func(i, j int) bool {
 		return ordered[i].Count > ordered[j].Count
 	})
+	out := bufio.NewWriterSize(os.Stdout, 64*1024)
 	for _, count := range ordered {
-		fmt.Println(count.Word, count.Count)
+		fmt.Fprintln(out, count.Word, count.Count)
 	}
+	out.Flush()
 }
 
-func increment(counts map[string]*int, word []byte) {
+func increment(counts map[string]*Count, word []byte) {
 	if p, ok := counts[string(word)]; ok {
-		// Word already in map, increment existing int via pointer.
-		*p++
-		return
+		// Word already in map, increment existing Count.
+		p.Count++
+	} else {
+		// Word not in map, insert new Count.
+		counts[string(word)] = &Count{string(word), 1}
 	}
-	// Word not in map, insert new int.
-	n := 1
-	counts[string(word)] = &n
 }
 
 type Count struct {
